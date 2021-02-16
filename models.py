e13b9706e1e300272931a55a8a4e8336cb4971e7
ameertg <ameer.saadat99@gmail.com>
2021-02-16 12:07:50 +0200

    First commit

--- models.py
+++ models.py
@@ -1,92 +1,70 @@
 import torch.nn as nn
+import torch.load as load
 import torch.nn.functional as F
 
-class ResidualBlock(nn.Module):
-    def __init__(self, in_features):
-        super(ResidualBlock, self).__init__()
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
-        conv_block = [  nn.ReflectionPad2d(1),
-                        nn.Conv2d(in_features, in_features, 3),
-                        nn.InstanceNorm2d(in_features),
-                        nn.ReLU(inplace=True),
-                        nn.ReflectionPad2d(1),
-                        nn.Conv2d(in_features, in_features, 3),
-                        nn.InstanceNorm2d(in_features)  ]
+def sample_gumbel(shape, eps=1e-20):
+    U = torch.rand(shape)
+    return -Variable(torch.log(-torch.log(U + eps) + eps))
 
-        self.conv_block = nn.Sequential(*conv_block)
+def gumbel_softmax_sample(logits, temperature):
+    y = logits + sample_gumbel(logits.size())
+    return F.softmax(y / temperature, dim=-1)
 
-    def forward(self, x):
-        return x + self.conv_block(x)
+def gumbel_softmax(logits, temperature):
+    """
+    input: [*, n_class]
+    return: [*, n_class] an one-hot vector
+    """
+    y = gumbel_softmax_sample(logits, temperature)
+    shape = y.size()
+    _, ind = y.max(dim=-1)
+    y_hard = torch.zeros_like(y).view(-1, shape[-1])
+    y_hard.scatter_(1, ind.view(-1, 1), 1)
+    y_hard = y_hard.view(*shape)
+    return (y_hard - y).detach() + y
 
 class Generator(nn.Module):
-    def __init__(self, input_nc, output_nc, n_residual_blocks=9):
+    def __init__(self, transformer):
         super(Generator, self).__init__()
 
-        # Initial convolution block       
-        model = [   nn.ReflectionPad2d(3),
-                    nn.Conv2d(input_nc, 64, 7),
-                    nn.InstanceNorm2d(64),
-                    nn.ReLU(inplace=True) ]
+        # Load transformer
+        transformer = torch.load(transformer, torch.device('cpu'))
+        self.model = transformer
 
-        # Downsampling
-        in_features = 64
-        out_features = in_features*2
-        for _ in range(2):
-            model += [  nn.Conv2d(in_features, out_features, 3, stride=2, padding=1),
-                        nn.InstanceNorm2d(out_features),
-                        nn.ReLU(inplace=True) ]
-            in_features = out_features
-            out_features = in_features*2
+    def forward(self, x):
+        seq = []
+        mems = tuple()
+        # Compute sequence of 128 chars
+        for i in range(128)
+            ret = self.model(data, target, *mems)
+            # Get probabilities of next token
+            probs, mems[i] = ret[0], ret[1:]
+            # Sample token with temeperature 0.1
+            next_tok = gumbel_softmax(probs, 0.1)
+            # Shift and concatenate next token
+            data = data[:, 1:]
+            data = data.concatenate(next_tok, axis=1)
+            print(next_tok)
 
-        # Residual blocks
-        for _ in range(n_residual_blocks):
-            model += [ResidualBlock(in_features)]
+        return data
 
-        # Upsampling
-        out_features = in_features//2
-        for _ in range(2):
-            model += [  nn.ConvTranspose2d(in_features, out_features, 3, stride=2, padding=1, output_padding=1),
-                        nn.InstanceNorm2d(out_features),
-                        nn.ReLU(inplace=True) ]
-            in_features = out_features
-            out_features = in_features//2
+class Discriminator(nn.Module, transformer):
+    def __init__(self, input_nc):
+        super(Discriminator, self).__init__()
 
-        # Output layer
-        model += [  nn.ReflectionPad2d(3),
-                    nn.Conv2d(64, output_nc, 7),
-                    nn.Tanh() ]
+        transformer = torch.load(transformer, torch.device('cpu'))
+        model = [   transformer,
+        nn.Linear(128, 50),
+        nn.LeakyRelu(),
+        nn.Linear(50, 2)
+        nn.Softmax(2)]
+
 
         self.model = nn.Sequential(*model)
 
     def forward(self, x):
         return self.model(x)
-
-class Discriminator(nn.Module):
-    def __init__(self, input_nc):
-        super(Discriminator, self).__init__()
-
-        # A bunch of convolutions one after another
-        model = [   nn.Conv2d(input_nc, 64, 4, stride=2, padding=1),
-                    nn.LeakyReLU(0.2, inplace=True) ]
-
-        model += [  nn.Conv2d(64, 128, 4, stride=2, padding=1),
-                    nn.InstanceNorm2d(128), 
-                    nn.LeakyReLU(0.2, inplace=True) ]
-
-        model += [  nn.Conv2d(128, 256, 4, stride=2, padding=1),
-                    nn.InstanceNorm2d(256), 
-                    nn.LeakyReLU(0.2, inplace=True) ]
-
-        model += [  nn.Conv2d(256, 512, 4, padding=1),
-                    nn.InstanceNorm2d(512), 
-                    nn.LeakyReLU(0.2, inplace=True) ]
-
-        # FCN classification layer
-        model += [nn.Conv2d(512, 1, 4, padding=1)]
-
-        self.model = nn.Sequential(*model)
-
-    def forward(self, x):
-        x =  self.model(x)
-        # Average pooling and flatten
-        return F.avg_pool2d(x, x.size()[2:]).view(x.size()[0], -1)
--- utils.py
+++ utils.py
@@ -8,11 +8,6 @@ import torch
 from visdom import Visdom
 import numpy as np
 
-def tensor2image(tensor):
-    image = 127.5*(tensor[0].cpu().float().numpy() + 1.0)
-    if image.shape[0] == 1:
-        image = np.tile(image, (3,1,1))
-    return image.astype(np.uint8)
 
 class Logger():
     def __init__(self, n_epochs, batches_epoch):
@@ -46,22 +41,16 @@ class Logger():
                 sys.stdout.write('%s: %.4f | ' % (loss_name, self.losses[loss_name]/self.batch))
 
         batches_done = self.batches_epoch*(self.epoch - 1) + self.batch
-        batches_left = self.batches_epoch*(self.n_epochs - self.epoch) + self.batches_epoch - self.batch 
+        batches_left = self.batches_epoch*(self.n_epochs - self.epoch) + self.batches_epoch - self.batch
         sys.stdout.write('ETA: %s' % (datetime.timedelta(seconds=batches_left*self.mean_period/batches_done)))
 
-        # Draw images
-        for image_name, tensor in images.items():
-            if image_name not in self.image_windows:
-                self.image_windows[image_name] = self.viz.image(tensor2image(tensor.data), opts={'title':image_name})
-            else:
-                self.viz.image(tensor2image(tensor.data), win=self.image_windows[image_name], opts={'title':image_name})
 
         # End of epoch
         if (self.batch % self.batches_epoch) == 0:
             # Plot losses
             for loss_name, loss in self.losses.items():
                 if loss_name not in self.loss_windows:
-                    self.loss_windows[loss_name] = self.viz.line(X=np.array([self.epoch]), Y=np.array([loss/self.batch]), 
+                    self.loss_windows[loss_name] = self.viz.line(X=np.array([self.epoch]), Y=np.array([loss/self.batch]),
                                                                     opts={'xlabel': 'epochs', 'ylabel': loss_name, 'title': loss_name})
                 else:
                     self.viz.line(X=np.array([self.epoch]), Y=np.array([loss/self.batch]), win=self.loss_windows[loss_name], update='append')
@@ -74,7 +63,7 @@ class Logger():
         else:
             self.batch += 1
 
-        
+
 
 class ReplayBuffer():
     def __init__(self, max_size=50):
@@ -107,12 +96,3 @@ class LambdaLR():
 
     def step(self, epoch):
         return 1.0 - max(0, epoch + self.offset - self.decay_start_epoch)/(self.n_epochs - self.decay_start_epoch)
-
-def weights_init_normal(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
-        torch.nn.init.normal(m.weight.data, 0.0, 0.02)
-    elif classname.find('BatchNorm2d') != -1:
-        torch.nn.init.normal(m.weight.data, 1.0, 0.02)
-        torch.nn.init.constant(m.bias.data, 0.0)
-
