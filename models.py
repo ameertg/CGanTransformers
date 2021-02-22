955ae7df12da40be65828b889f1a198050e671d9
ameertg <ameer.saadat99@gmail.com>
2021-02-22 10:23:59 +0200

    Added recon loss network

--- models.py
+++ models.py
@@ -2,68 +2,118 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch
 from torch.autograd import Variable
+import math
 
-def sample_gumbel(shape, eps=1e-20):
-    U = torch.rand(shape)
-    return -Variable(torch.log(-torch.log(U + eps) + eps))
+class Sampler(object):
+    def sample_gumbel(shape, eps=1e-20):
+        U = torch.rand(shape)
+        return -Variable(torch.log(-torch.log(U + eps) + eps))
 
-def gumbel_softmax_sample(logits, temperature):
-    y = logits + sample_gumbel(logits.size())
-    return F.softmax(y / temperature, dim=-1)
+    def gumbel_softmax_sample(logits, temperature):
+        y = logits + sample_gumbel(logits.size())
+        return F.softmax(y / temperature, dim=-1)
 
-def gumbel_softmax(logits, temperature):
-    """
-    input: [*, n_class]
-    return: [*, n_class] an one-hot vector
-    """
-    y = gumbel_softmax_sample(logits, temperature)
-    shape = y.size()
-    _, ind = y.max(dim=-1)
-    y_hard = torch.zeros_like(y).view(-1, shape[-1])
-    y_hard.scatter_(1, ind.view(-1, 1), 1)
-    y_hard = y_hard.view(*shape)
-    return torch.nonzero((y_hard - y).detach() + y)
+    def gumbel_softmax(logits, temperature):
+        """
+        input: [*, n_class]
+        return: [*, n_class] an one-hot vector
+        """
+        y = gumbel_softmax_sample(logits, temperature)
+        shape = y.size()
+        _, ind = y.max(dim=-1)
+        y_hard = torch.zeros_like(y).view(-1, shape[-1])
+        y_hard.scatter_(1, ind.view(-1, 1), 1)
+        y_hard = y_hard.view(*shape)
+        return (y_hard - y).detach() + y
+
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x)
+
+class TransformerModel(nn.Module):
+
+    def __init__(self, ntoken, ninp, nhead, nhid, nlayers, dropout=0.5):
+        super(TransformerModel, self).__init__()
+        from torch.nn import TransformerEncoder, TransformerEncoderLayer
+        self.model_type = 'Transformer'
+        self.pos_encoder = PositionalEncoding(ninp, dropout)
+        encoder_layers = TransformerEncoderLayer(ninp, nhead, nhid, dropout)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
+        self.encoder = nn.Embedding(ntoken, ninp)
+        self.ninp = ninp
+        self.decoder = nn.Linear(ninp, ntoken)
+
+        self.init_weights()
+
+    def init_weights(self):
+        initrange = 0.1
+        self.encoder.weight.data.uniform_(-initrange, initrange)
+        self.decoder.bias.data.zero_()
+        self.decoder.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, src):
+        src = self.encoder(src) * math.sqrt(self.ninp)
+        src = self.pos_encoder(src)
+        output = self.transformer_encoder(src)
+        output = self.decoder(output)
+        return output
+
+
 
 class Generator(nn.Module):
-    def __init__(self, transformer):
+    def __init__(self):
         super(Generator, self).__init__()
 
-        # Load transformer
-        self.model = torch.load(transformer, torch.device('cpu'))
 
-    def forward(self, x, bptt):
-        seq = []
-        mems = tuple()
-        # Compute sequence of 128 tokens
-        for i in range(bptt):
-            ret = self.model._forward(x, *mems)
-            # Get probabilities of next token
-            probs, mems = ret[0], ret[1:]
-            # Sample token with temeperature 0.1
-            next_tok = gumbel_softmax(probs[-1, :, :], 0.1)[:, 1].unsqueeze(0)
-            # Shift and concatenate next token
-            x = x[1:, :]
-            x = torch.cat([x, next_tok], 0)
+        self.custom_transformer = True
+        self.model = TransformerModel(631, 40, 8, 120, 6)
 
+    def forward(self, x, bptt, eos):
+        x = self.model(x)
+        x = Sampler.gumbel_softmax(x, 0.1)
+        x = torch.nonzero(x, as_tuple=True)[2].reshape(12, 2)
+        x = x
         return x
 
 class Discriminator(nn.Module):
     def __init__(self, transformer):
         super(Discriminator, self).__init__()
 
-        self.transformer = torch.load(transformer, torch.device('cpu'))
-        model = [nn.Linear(512, 50),
-        nn.ReLU(),
-        nn.Linear(50, 1)]
 
-
-        self.model = nn.Sequential(*model)
-        self.final = nn.Linear(12, 1)
+        self.custom_transformer = True
+        self.model = TransformerModel(631, 40, 8, 120, 6)
+        self.linear_out = nn.Linear(631, 1)
 
     def forward(self, x):
-        mems = tuple()
-        x = self.transformer._forward(x, *mems)[0]
-        x = x.transpose(0,1)
-        x = self.model(x).squeeze(2)
-        x = self.final(x)
-        return torch.sigmoid(x)
+        x = self.model(x)
+        x = self.linear_out(x)
+        return F.sigmoid(x)
+
+class CycleLoss(nn.Module):
+
+    def __init__(self, transformer):
+        super(Discriminator, self).__init__()
+
+        self.model = TransformerModel(631, 80, 8, 160, 6)
+        self.linear_out = nn.Linear(631, 1)
+
+    def forward(self, x, y, label):
+            x = x.cat(y)
+            x = self.model(x)
+            x = self.linear_out(x)
+        return F.sigmoid(x)
--- train.py
+++ train.py
@@ -28,7 +28,6 @@ parser.add_argument('--input_nc', type=int, default=3, help='number of channels 
 parser.add_argument('--output_nc', type=int, default=3, help='number of channels of output data')
 parser.add_argument('--cuda', action='store_true', help='use GPU computation')
 parser.add_argument('--n_cpu', type=int, default=8, help='number of cpu threads to use during batch generation')
-parser.add_argument('--transformer', type=str, default='model.pt', help='pre-trained transformer')
 
 opt = parser.parse_args()
 print(opt)
@@ -38,10 +37,13 @@ if torch.cuda.is_available() and not opt.cuda:
 
 ###### Definition of variables ######
 # Networks
-netG_A2B = Generator(opt.transformer)
-netG_B2A = Generator(opt.transformer)
-netD_A = Discriminator(opt.transformer)
-netD_B = Discriminator(opt.transformer)
+netG_A2B = Generator()
+netG_B2A = Generator()
+netD_A = Discriminator()
+netD_B = Discriminator()
+netC = CycleLoss()
+
+cycle_loss = CycleLoss()
 
 if opt.cuda:
     netG_A2B.cuda()
@@ -50,31 +52,28 @@ if opt.cuda:
     netD_B.cuda()
 
 # Lossess
-criterion_GAN = torch.nn.MSELoss()
-criterion_cycle = torch.nn.L1Loss()
-criterion_identity = torch.nn.L1Loss()
+criterion_GAN = torch.nn.CrossEntropyLoss()
+criterion_cycle = torch.nn.CrossEntropyLoss()
 
 # Optimizers & LR schedulers
 optimizer_G = torch.optim.Adam(itertools.chain(netG_A2B.parameters(), netG_B2A.parameters()),
                                 lr=opt.lr, betas=(0.5, 0.999))
 optimizer_D_A = torch.optim.Adam(netD_A.parameters(), lr=opt.lr, betas=(0.5, 0.999))
 optimizer_D_B = torch.optim.Adam(netD_B.parameters(), lr=opt.lr, betas=(0.5, 0.999))
+optimizer_cycle = torch.optim.Adam(netD_B.parameters(), lr=opt.lr, betas=(0.5, 0.999))
 
 lr_scheduler_G = torch.optim.lr_scheduler.LambdaLR(optimizer_G, lr_lambda=LambdaLR(opt.n_epochs, opt.epoch, opt.decay_epoch).step)
 lr_scheduler_D_A = torch.optim.lr_scheduler.LambdaLR(optimizer_D_A, lr_lambda=LambdaLR(opt.n_epochs, opt.epoch, opt.decay_epoch).step)
 lr_scheduler_D_B = torch.optim.lr_scheduler.LambdaLR(optimizer_D_B, lr_lambda=LambdaLR(opt.n_epochs, opt.epoch, opt.decay_epoch).step)
+lr_scheduler_recon = torch.optim.lr_scheduler.LambdaLR(optimizer_cycle, lr_lambda=LambdaLR(opt.n_epochs, opt.epoch, opt.decay_epoch).step)
 
 # Inputs & targets memory allocation
 Tensor = torch.cuda.FloatTensor if opt.cuda else torch.Tensor
 target_real = Variable(Tensor(opt.batchSize).fill_(1.0), requires_grad=False)
 target_fake = Variable(Tensor(opt.batchSize).fill_(0.0), requires_grad=False)
 
-fake_A_buffer = ReplayBuffer()
-fake_B_buffer = ReplayBuffer()
-
-
-nes_corpus = get_lm_corpus('data/nesmdb_tx10', 'nesmdb')
-nes_corpus_iter = nes_corpus.get_iterator('train', bsz=2, bptt=12)
+nes_corpus = get_lm_corpus('data/nesmdb_tx1', 'nesmdb')
+nes_corpus_iter = nes_corpus.get_iterator('train', bsz=1, bptt=12)
 lakh_corpus_iter = nes_corpus_iter
 
 # Loss plot
@@ -92,14 +91,6 @@ for epoch in range(0, 100):
         ###### Generators A2B and B2A ######
         optimizer_G.zero_grad()
 
-        # Identity loss
-        # G_A2B(B) should equal B if real B is fed
-        same_B = netG_A2B(real_B, bptt)
-        loss_identity_B = criterion_identity(same_B.float(), real_B.float())*5.0
-        # G_B2A(A) should equal A if real A is fed
-        same_A = netG_B2A(real_A, bptt)
-        loss_identity_A = criterion_identity(same_A.float(), real_A.float())*5.0
-
         # GAN loss
         fake_B = netG_A2B(real_A, bptt)
         pred_fake = netD_B(fake_B)
@@ -111,14 +102,13 @@ for epoch in range(0, 100):
 
         # Cycle loss
         recovered_A = netG_B2A(fake_B, bptt)
-        loss_cycle_ABA = criterion_cycle(recovered_A.float(), real_A.float())*10.0
+        loss_cycle_ABA = netC(real_A, recovered_A)*10.0
 
         recovered_B = netG_A2B(fake_A, bptt)
-        loss_cycle_BAB = criterion_cycle(recovered_B.float(), real_B.float())*10.0
+        loss_cycle_BAB = netC(real_A, recovered_A)*10.0
 
         # Total loss
-        loss_G = loss_identity_A + loss_identity_B + loss_GAN_A2B + loss_GAN_B2A + loss_cycle_ABA + loss_cycle_BAB
-        print(loss_G)
+        loss_G = loss_GAN_A2B + loss_GAN_B2A + loss_cycle_ABA + loss_cycle_BAB
         loss_G.backward()
         optimizer_G.step()
         ###################################
@@ -131,7 +121,6 @@ for epoch in range(0, 100):
         loss_D_real = criterion_GAN(pred_real, target_real)
 
         # Fake loss
-        fake_A = fake_A_buffer.push_and_pop(fake_A)
         pred_fake = netD_A(fake_A.detach())
         loss_D_fake = criterion_GAN(pred_fake, target_fake)
 
@@ -150,7 +139,6 @@ for epoch in range(0, 100):
         loss_D_real = criterion_GAN(pred_real, target_real)
 
         # Fake loss
-        fake_B = fake_B_buffer.push_and_pop(fake_B)
         pred_fake = netD_B(fake_B.detach())
         loss_D_fake = criterion_GAN(pred_fake, target_fake)
 
@@ -161,15 +149,42 @@ for epoch in range(0, 100):
         optimizer_D_B.step()
         ###################################
 
+        ###### Cycle loss ######
+        optimizer_cycle.zero_grad()
+
+        # A-A
+        pred_real = CycleLoss(real_A.detach(), real_A.detach())
+        loss_real_A = criterion_cycle(pred_real, target_real)
+
+        # A-A*
+        pred_fake = CycleLoss(real_A.detach(), fake_A.detach())
+        loss_fake_A = criterion_cycle(pred_fake, target_fake)
+
+        # B-B
+        pred_real = CycleLoss(real_B.detach(), real_B.detach())
+        loss_real_B = criterion_cycle(pred_real, target_real)
+
+        # B-B*
+        pred_fake = CycleLoss(real_B.detach(), fake_B.detach())
+        loss_fake_B = criterion_cycle(pred_fake, target_fake)
+
+        # Total loss
+        loss_cycle = (loss_real_A + loss_real_B + loss_fake_A + loss_fake_B)*0.25
+        loss_cycle.backward()
+
+        optimizer_cycle.step()
+        ###################################
+
 
     # Update learning rates
     lr_scheduler_G.step()
     lr_scheduler_D_A.step()
     lr_scheduler_D_B.step()
 
-    # # Save models checkpoints
-    # torch.save(netG_A2B.state_dict(), 'output/netG_A2B.pth')
-    # torch.save(netG_B2A.state_dict(), 'output/netG_B2A.pth')
-    # torch.save(netD_A.state_dict(), 'output/netD_A.pth')
-    # torch.save(netD_B.state_dict(), 'output/netD_B.pth')
+    # Save models checkpoints
+    torch.save(netG_A2B.state_dict(), 'output/netG_A2B.pth')
+    torch.save(netG_B2A.state_dict(), 'output/netG_B2A.pth')
+    torch.save(netD_A.state_dict(), 'output/netD_A.pth')
+    torch.save(netD_B.state_dict(), 'output/netD_B.pth')
+    torch.save(netC.state_dict(), 'output/netC.pth')
 ###################################
